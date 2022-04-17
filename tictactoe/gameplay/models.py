e28f6d296ba84d7c757a187bdf2f433c7c54b311
alexqiafuer <alexqiafuer@gmail.com>
2022-04-17 15:32:56 -0500

    Finished css style

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.db import models
 from django.db.models import Q
 from django.contrib.auth.models import User
--- tictactoe/tictactoe/views.py
+++ tictactoe/tictactoe/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def welcome(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'tictactoe/welcome.html')

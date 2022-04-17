8e9d734f7ee7b9118545e724cd0590544808c011
alexqiafuer <alexqiafuer@gmail.com>
2022-04-16 22:16:21 -0500

    Added gameplay, player, move apps

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,13 +1,35 @@
 from pyexpat import model
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import User
+
+GAME_STATUS_CHOICES = (
+    ('F', 'First player to Move'),
+    ('S', 'Second player to Move'),
+    ('W', 'First player Wins'),
+    ('L', 'Second player Wins'),
+    ('D', 'Draw')
+)
+
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+        return self.filter(
+            Q(first_player=user) or Q(second_player=user)
+        )
+    
+    def active(self):
+        return self.filter(
+            Q(status='F') or Q(status='S')
+        )
 
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="game_player_1", on_delete=models.CASCADE)
     second_player = models.ForeignKey(User, related_name="game_player_2", on_delete=models.CASCADE)
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, default='F')
+    status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+    objects = GamesQuerySet.as_manager()
 
     def __str__(self):
         return f"{self.first_player} vs {self.second_player}"
--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import welcome
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path('', welcome)
+    path('admin/', admin.site.urls),
+    path('', welcome),
+    path('player/', include('player.urls'))
 ]

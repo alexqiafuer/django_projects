21672d0c7084490e7f0772fc67d617e370366cd0
alexqiafuer <alexqiafuer@gmail.com>
2022-04-21 16:42:46 -0500

    finished demo

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,3 +1,10 @@
+from operator import mod
+from pyexpat import model
+from tracemalloc import is_tracing
+from xmlrpc.client import boolean
 from django.db import models
 
-# Create your models here.
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+    is_true = models.BooleanField(default=True)
--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,10 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+from .models import Feature
 
 def index(request):
-    return render(request, 'index.html')
+    features = Feature.objects.all()
+
+    return render(request, 'index.html', {'features':features})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        pswd = request.POST['password']
+        pswd2 = request.POST['password2']
+        if pswd == pswd2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already registered!')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username is taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=pswd)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            # user invalid
+            messages.info(request, 'Please check your username and password')
+            return redirect('login')
+
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 def counter(request):
     text = request.POST['text']
     word_counts = len(text.split())
     return render(request, 'counter.html', {'counts':  word_counts})
+
+def post(request, pk):
+    return render(request, 'post.html', {'pk':pk})

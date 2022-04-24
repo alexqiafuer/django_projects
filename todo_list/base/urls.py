705c387128156e35c6525b31d18a419f080ba7c6
alexqiafuer <alexqiafuer@gmail.com>
2022-04-24 00:21:22 -0500

    finished login/logout

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskDetail, TaskList, TaskCreate, TaskUpdate, TaskDelete, CustomLogin, RegisterUser
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLogin.as_view(), name='login' ),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<str:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
